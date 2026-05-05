f71836e9828c3ac30346f0a419b607d25ecd9f58
ronak bihani <ronakbihani123@gmail.com>
2026-05-05 10:44:21 +0530

    training done

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-# from src.MLClassifier_P import logger 
+# from src.cnnClassifier import logger 
 # import logging 
 # print(logging.info("Welcome to ML Batch P logs"))
-
-from src.MLClassifier_P import logger
-from src.MLClassifier_P.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.cnnClassifier.entity.config_entity import TrainingConfig
+from src.cnnClassifier import logger
+from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline 
+from src.cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -11,6 +13,28 @@ try:
     logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare Base Model"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Training"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
     logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
